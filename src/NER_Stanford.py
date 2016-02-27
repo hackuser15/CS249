97bf129c13312e6e7b9361160062f815056d508f
Akshay Dattatray Shinde <Akshay Dattatray Shinde>
2016-02-27 04:00:43 -0800

    Trained model on product data

--- src/NER_Stanford.py
+++ src/NER_Stanford.py
@@ -6,15 +6,93 @@
 import os
 import nltk
 from nltk.tag import StanfordNERTagger
+from nltk.internals import find_jars_within_path
+import json
+import pandas as pd
+
+def checkWithinRange(num,list):
+    for x in list:
+        elem = x.split('-')
+        if len(elem) == 1:
+            if num == int(elem[0]):
+                return  True
+        elif len(elem) == 2:
+            start, end = map(int,elem)
+            if start <= num <= end:
+                return True
+        else: # more than one hyphen
+            return False
+    return False
+
+def assignProductLabel(row):
+    if(checkWithinRange(row['tokenid'],row['occurence'])):
+        return 'prod'
+    else:
+        return 'O'
+def assignDummyProductLabel(row):
+    return 'O'
+
+with open('training-annotated.json') as data_file:
+    ann_textItems = json.load(data_file)
+
+test_data = pd.read_csv('test.csv')
+
+ann_textItems = pd.DataFrame(list(ann_textItems['TextItem'].items()))
+ann_textItems.columns = ['docid', 'text']
+
+
+dsmb_textItems = pd.read_csv('training-disambiguated-product-mentions.csv')
+dsmb_textItems = dsmb_textItems.drop('documents',1)
+dsmb_textItems = dsmb_textItems['id'].apply(lambda x: pd.Series(x.split(':')))
+dsmb_textItems.columns = ['docid', 'occurence']
+dsmb_textItems = dsmb_textItems.groupby('docid', as_index=False).agg({'occurence': lambda x: list(x)})
+
+ann_textItems = pd.merge(ann_textItems, dsmb_textItems, on = 'docid')
+
+rows = []
+ann_textItems.apply(lambda row: [rows.append([row['docid'], txt, index, row['occurence']]) for index, txt in enumerate(row.text)], axis=1)
+ann_textItems_new = pd.DataFrame(rows, columns=['docid','token','tokenid','occurence'])
+
+ann_textItems_new['label']= ann_textItems_new.apply (lambda row: assignProductLabel (row),axis=1)
+ann_textItems_new['dummy']= ann_textItems_new.apply (lambda row: assignDummyProductLabel (row),axis=1)
+stanford_train = ann_textItems_new[['token','label']]
+stanford_test = ann_textItems_new[['token','dummy']]
+
+# stanford_train.to_csv('ner_stanford_train_products', sep='\t', header=False , index=False)  #Training data for model
+# stanford_test.to_csv('ner_stanford_test_products', sep='\t', header=False , index=False)    #dummy data testing on training set
 
 path_stanford = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '\stanford-ner-2015-12-09'
-path_to_model = path_stanford + '\classifiers\english.all.3class.distsim.crf.ser.gz'
+# path_to_model = path_stanford + '\classifiers\english.all.3class.distsim.crf.ser.gz'
+path_to_model = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '\stanford.ner-model.products.gz'   #This is the model we trained on products data
 path_to_jar = path_stanford + '\stanford-ner.jar'
 st = StanfordNERTagger(path_to_model, path_to_jar)
 
-# stanford_dir = st._stanford_jar.rpartition('\\')[0]
-from nltk.internals import find_jars_within_path
+stanford_dir = st._stanford_jar.rpartition('\\')[0]
+
 stanford_jars = find_jars_within_path(path_stanford)
 st._stanford_jar = ';'.join(stanford_jars)
+print(st._stanford_jar)
+print(st.tag(nltk.word_tokenize(test_data['Value'][50])))
 
-print(st.tag(nltk.word_tokenize('Akshay Shinde is studying at University of California, Los Angeles')))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
