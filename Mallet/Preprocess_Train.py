dcda4a1e307f7fe896ef361514fdf5baf066e53f
Akshay Dattatray Shinde <Akshay Dattatray Shinde>
2016-03-02 00:29:10 -0800

    Added code to add blank line after TextItems

--- Mallet/Preprocess_Train.py
+++ Mallet/Preprocess_Train.py
@@ -1,5 +1,4 @@
 import json
-
 import pandas as pd
 import string
 from bs4 import BeautifulSoup
@@ -35,6 +34,7 @@ ann_textItems_new = pd.DataFrame(rows, columns=['docid','token','tokenid'])
 product_terms = set()
 #print(dsmb_textItems)
 
+# Creating set of products
 for index, row in dsmb_textItems.iterrows():
     textItemId = row['docid']
     range_list = row['range']
@@ -45,34 +45,42 @@ for index, row in dsmb_textItems.iterrows():
         df = ann_textItems_new[ann_textItems_new['docid'] == textItemId][start:end+1]
         # l1 = list(df['token'])
         l1 = [x for x in list(df['token']) if not x.isdigit()]
-        print(textItemId,":",l1)
+        # print(textItemId,":",l1)
         product_terms.update(l1[0:])
-
-#print(ann_textItems_new)
 
 ann_textItems_new['label']= ann_textItems_new.apply (lambda row: assignProductLabel (row),axis=1)
 ann_textItems_new['dummy']= ann_textItems_new.apply (lambda row: assignDummyProductLabel (row),axis=1)
-stanford_train = ann_textItems_new[['token','label']]
-stanford_test = ann_textItems_new[['token','dummy']]
+
+# Adding blank row after reach text item
+prev_docid = ''
+for i, row in ann_textItems_new.iterrows():
+    curr_docid = row['docid']
+    if(i > 0 and i < len(ann_textItems_new)-1 and curr_docid != prev_docid):
+        df = ann_textItems_new[0:i]
+        df = df.append({"docid": "","token":"","tokenid":""},ignore_index=True)
+        df = df.append(ann_textItems_new[i:])
+        ann_textItems_new = df
+    prev_docid = curr_docid
 
 #Data cleaning
-stop = stopwords.words('english')
-indexes = []
-for i in range(0, len(ann_textItems_new['token'])):
-    text = ann_textItems_new['token'][i]
-    text = text.lower()
-    if text in stop:
-        indexes.append(i)
-        continue
-    text = BeautifulSoup(text,"lxml").get_text()
-    text = "".join([ch for ch in text if ch not in string.punctuation])
-    if not text:
-        indexes.append(i)
-ann_textItems_new.drop(ann_textItems_new.index[indexes],inplace=True)
+# stop = stopwords.words('english')
+# indexes = []
+# for i in range(0, len(ann_textItems_new['token'])):
+#     text = ann_textItems_new['token'][i]
+#     if not text:
+#         continue
+#     text = text.lower()
+#     if text in stop:
+#         indexes.append(i)
+#         continue
+#     text = BeautifulSoup(text,"lxml").get_text()
+#     text = "".join([ch for ch in text if ch not in string.punctuation])
+#     if not text:
+#         indexes.append(i)
+# ann_textItems_new.drop(ann_textItems_new.index[indexes],inplace=True)
+
 stanford_train = ann_textItems_new[['token','label']]
 stanford_test = ann_textItems_new[['token','dummy']]
 
 stanford_train.to_csv('ner_stanford_train_product_new2', sep=' ', header=False , index=False)  #Training data for model
 stanford_test.to_csv('ner_stanford_test_product_new2', sep=' ', header=False , index=False)    #dummy data testing on training set
-
-print(product_terms)
