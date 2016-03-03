06cc1d61f2a5a91bb9f87042fc4d71348a524c7e
Akshay Dattatray Shinde <Akshay Dattatray Shinde>
2016-03-02 21:50:42 -0800

    Prediction with identified products

--- src/NER_Stanford.py
+++ src/NER_Stanford.py
@@ -101,48 +101,56 @@ def predictLabelStanford(test_data):
         op = st.tag(test_data[test_data['docid']==doc]['token'])
         op = pd.DataFrame(op, columns=['token','pred_label'])
         op['docid'] = pd.Series([doc for x in range(len(op.index))], index=op.index)
+        op['tokenid'] = pd.Series([x for x in range(len(op.index))], index=op.index)
         prediction=prediction.append(op,ignore_index=True)
     return prediction
 
 def getProductOccurence(prediction):
     final_list = pd.DataFrame(columns=['docid','occurences'])
-    text_index = 0
-    index_str="";
     seperator ="-"
     l = []
+    product = ''
+    lproduct = []
     i = 0
     prev_docid = ''
     while(i < len(prediction['pred_label'])):
         curr_docid = prediction['docid'][i]
         label = prediction['pred_label'][i]
         if(i>0 and curr_docid != prev_docid ):
-            final_list=processTextItem(l, prev_docid, final_list)
-            text_index = 0
+            final_list=processTextItem(l, lproduct, prev_docid, final_list)
             l.clear()
-            i = i+1
-            prev_docid = curr_docid
-        elif(label == 'prod'):
+            lproduct.clear()
+        if(label == 'prod'):
             start_index = prediction['tokenid'][i]
+            product = ''
+            token = prediction['token'][i]
             end_index = start_index
-            while(label == 'prod' and curr_docid == prev_docid):
+            while((label == 'prod' and curr_docid == prev_docid) or start_index == end_index):
                 prev_docid = curr_docid
+                product += " "+token
                 i = i+1
+                if(i==len(prediction)):
+                    l.append(str(start_index)+seperator+str(end_index))
+                    lproduct.append(product)
+                    break
                 curr_docid = prediction['docid'][i]
-                end_index+=1
-                text_index+=1
+                end_index = prediction['tokenid'][i] if (curr_docid == prev_docid or end_index == 0) else prediction['tokenid'][i-1]+1
                 label = str(prediction['pred_label'][i])
+                token = prediction['token'][i]
             l.append(str(start_index)+seperator+str(end_index-1))
+            lproduct.append(product)
         elif(label == 'O'):
-            text_index +=1
             i = i+1
             prev_docid = curr_docid
-    final_list=processTextItem(l, prediction['docid'][i-1], final_list)
+    final_list=processTextItem(l, lproduct, prediction['docid'][i-1], final_list)
     return final_list
 
-def processTextItem(l, textItemID, final_list):
+def processTextItem(l, lproduct, textItemID, final_list):
     if(len(l) > 0):
-        r = l[0]
-        final_list=final_list.append({"docid":textItemID,"occurences":r},ignore_index=True)
+        op = pd.DataFrame(l, columns=['occurences'])
+        op['product'] = lproduct
+        op['docid'] = pd.Series([textItemID for x in range(len(op.index))], index=op.index)
+        final_list=final_list.append(op, ignore_index=True)
     return final_list
 
 with open('training-annotated.json') as data_file:
@@ -179,9 +187,11 @@ train_data = train_data[['token','label']]
 test_data = test_data[['docid','token','label']]
 
 # train_data.to_csv('ner_train_data', sep='\t', header=False , index=False)  #Training data for model
-test_data.to_csv('ner_test_data', sep='\t', header=False , index=False)    #dummy data testing on training set
+test_data.to_csv('ner_test_data', sep='\t', header=False , index=False)
 
 #Prediction
 pred_data = predictLabelStanford(test_data)
+pred_data.to_csv('ner_pred_data', sep='\t', header=False , index=False)    #dummy data testing on training set
 pred_data = getProductOccurence(pred_data)
 print(pred_data)
+# pred_data['product'].to_csv('ner_pred_data', sep='\t', header=False , index=False)    #dummy data testing on training set
