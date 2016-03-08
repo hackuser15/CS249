6c798ea0f1a5250cfe57809ddbb5dd2fc5bd7d14
Ashish Nerkar <ashishnerkar1@gmail.com>
2016-03-08 01:06:26 -0800

    Added code to generate  training and test data

--- Dataset/generate_test_data.py
+++ Dataset/generate_test_data.py
@@ -1,7 +1,7 @@
 import numpy
 import pandas as pd
 import random
-
+import json
 
 input_data = pd.read_csv('training-disambiguated-product-mentions.csv')
 
@@ -11,9 +11,35 @@ df_test = df.sample(n=10)
 
 df_train = df.loc[~df.index.isin(df_test)]
 
-df_test.to_csv('test-disambiguated',header=False,index=None)
-df_train.to_csv('train_disambiguated',header=False,index=None)
+'''
+test_list = df_test['id'].tolist()
+print test_list
+
+id_list = []
+for str in test_list:
+    id = str.split(':')
+    id_list.append(id)
+
+with open('training-annotated-text.json') as data_file:
+    ann_textItems = json.load(data_file)
+
+remove_list = []
+for i in xrange(len(ann_textItems)):
+    item = ann_textItems['TextItem'][i]
+#    if ann_textItems[i]['TextItem'] in id_list:
+        #ann_textItems.pop(i)
+#        remove_list.append(i)
+    print  item
+#ann_textItems = pd.DataFrame(list(ann_textItems['TextItem'].items()))
+
+ann_textItems.pop(remove_list)
+
+'''
+df_test.to_csv('test-disambiguated.csv',header=False,index=None)
+df_train.to_csv('train_disambiguated.csv',header=False,index=None)
+'''
 
 
-
-
+with open('trial-training-data.json','w') as output:
+    json.dump(ann_textItems, output)
+'''
